3a3b9667183bc91fe33815fc0aeffa672e00223d
Kyyy12345 <rladbsduf135@naver.com>
2026-06-24 23:09:55 +0900

    router 설정 및 레이아웃 설정

--- src/pages/aboutPage.tsx
+++ src/pages/aboutPage.tsx
@@ -1,8 +1,12 @@
+import { NavLink } from "react-router";
 
 
 function AboutPage() {
     return (
         <div className=" text-3xl">
+            <div className="flex">
+                <NavLink to='/'>Main</NavLink>
+            </div>
             <div>About Page</div>
         </div>
     );
